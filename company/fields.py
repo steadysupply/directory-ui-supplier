f8e0c62d9d024a8856e42ffb436e5e1d3b0f1684
Richard Tier <rihard@highertier.com>
2017-07-17 16:03:19 +0100

    Improve documentation of SeoFriendlyChoiceField

--- company/fields.py
+++ company/fields.py
@@ -2,6 +2,14 @@ from django import forms
 
 
 class SeoFriendlyChoiceField(forms.ChoiceField):
+    """
+    The field value is exposed in the URL when then form is submitted.
+    `sector=FOOD_AND_DRINK`, for example, would not look great visually, and
+    there are SEO implications. Functionality added to this field allows the
+    url to show `food-and-drink`, and it will be converted to `FOOD_AND_DRINK`
+
+    """
+
     def to_python(self, value):
         value = super().to_python(value)
         return value.replace('-', '_').upper()
